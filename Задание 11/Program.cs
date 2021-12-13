624b7947a11452938f7b9a39057bbb8822b813a4
andrey <andrey@0D6C>
2021-12-13 22:38:30 +0300

    Добавьте файлы проекта.

---
+++ Задание 11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    /*Разработать структуру для решения линейного уравнения 0=kx+b. 
+     *Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+     *Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. 
+     *Осуществить использование экземпляра в программе.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation { k = 100, b = -10 };
+            equation.Root();
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double k;
+            public double b;
+            public void Root()
+            {
+                double x = -b / k;
+                Console.WriteLine("x = {0:f2}", x);
+            }
+        }
+    }
+}
